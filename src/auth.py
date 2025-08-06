d40311a9cd9350ea540bd5c047db3f4586bd21d2
Brandon Dunbar <brandon.dunbar97@gmail.com>
2025-08-06 17:45:22 -0400

    feat(auth): Implemented get_service_account_credentials to simplify auth handling.
    Signed-off-by: Brandon Dunbar <brandon.dunbar97@gmail.com>

---
+++ src/auth.py
@@ -0,0 +1,24 @@
+from google.oauth2.service_account import Credentials
+from src.constants import GOOGLE_CREDENTIALS_PATH
+
+
+def get_service_account_credentials(scopes=None) -> Credentials:
+    """
+    Load and return Google service account credentials with the specified scopes.
+
+    Args:
+        scopes (list, optional): List of OAuth2 scopes. Defaults to basic Sheets and Docs scopes.
+
+    Returns:
+        Credentials: Authenticated service account credentials.
+    """
+    if scopes is None:
+        scopes = [
+            "https://www.googleapis.com/auth/spreadsheets.readonly",
+            "https://www.googleapis.com/auth/documents"
+        ]
+
+    return Credentials.from_service_account_file(
+        GOOGLE_CREDENTIALS_PATH,
+        scopes=scopes
+    )
