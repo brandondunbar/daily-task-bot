e9db8fc14c15962e5358f14db2a8fb26c8ab796f
Brandon Dunbar <brandon.dunbar97@gmail.com>
2025-08-06 15:39:41 -0400

    chore(main): Removed calendar logic.
    Signed-off-by: Brandon Dunbar <brandon.dunbar97@gmail.com>

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from src.config_loader import load_config
 from src.google_sheets import get_sheet_rows
 from src.scheduler import find_today_task
 from src.google_docs import create_google_doc
-from src.google_calendar import create_calendar_event
 
 
 def main():
@@ -17,7 +16,6 @@ def main():
         return
 
     doc_link = create_google_doc(sheet_conf, task)
-    create_calendar_event(config, task, doc_link)
 
 
 if __name__ == "__main__":
