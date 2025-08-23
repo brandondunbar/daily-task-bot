90ebb663ba1e4cce69a40410a9b10f6d06411033
Brandon Dunbar <brandon.dunbar97@gmail.com>
2025-08-23 17:57:46 -0400

    feat(auth): Added logging.
    Signed-off-by: Brandon Dunbar <brandon.dunbar97@gmail.com>

--- src/auth.py
+++ src/auth.py
@@ -7,6 +7,9 @@ a service account JSON file and optional scopes for Google APIs.
 from google.oauth2.service_account import Credentials
 
 from src.constants import GOOGLE_CREDENTIALS_PATH
+from src.observability.logging_setup import get_logger
+
+log = get_logger(__name__)
 
 
 def get_service_account_credentials(scopes=None) -> Credentials:
@@ -25,7 +28,16 @@ def get_service_account_credentials(scopes=None) -> Credentials:
             "https://www.googleapis.com/auth/documents"
         ]
 
-    return Credentials.from_service_account_file(
-        GOOGLE_CREDENTIALS_PATH,
-        scopes=scopes
-    )
+    try:
+        creds = Credentials.from_service_account_file(
+            GOOGLE_CREDENTIALS_PATH,
+            scopes=scopes
+        )
+        log.info("credentials_loaded", path=GOOGLE_CREDENTIALS_PATH, scopes=scopes)
+        return creds
+    except Exception as e:
+        log.exception(
+            "credentials_load_failed",
+            path=GOOGLE_CREDENTIALS_PATH,
+            error=str(e))
+        raise
